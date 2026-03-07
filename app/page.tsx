e434d74eaee1d17808f802da702b4fa8a33c9122
Andreas Thune <andy_19955@hotmail.com>
2026-03-07 23:55:24 +0100

    Add searchbar with filtering functionality

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,10 @@ export default function Home() {
     <>
       <Header />
       <main className="flex-1 flex justify-center">
-        <div className="max-w-7xl w-full py-16 px-6">
+        <div className="max-w-7xl w-full pt-8 pb-16 px-6">
+          <div className="mb-8">
+            <FetchProducts showGrid={false} />
+          </div>
           <section className="mb-12 grid grid-cols-1 gap-8 md:grid-cols-2 items-center">
             <div>
               <h1 className="text-5xl font-extrabold mb-4">Discover standout products</h1>
