e40c871f428b4ebec655b51ee2a48c4f6f91c5e7
Andreas Thune <andy_19955@hotmail.com>
2026-03-07 20:25:17 +0100

    Project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-6">Welcome to The Online Store</h1>
+    </>
+  );
+}
---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["static.noroff.dev", "images.unsplash.com"],
+  },
+};
+
+export default nextConfig;
