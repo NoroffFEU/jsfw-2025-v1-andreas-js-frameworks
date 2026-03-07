249067996e56f7e69327be1656785a52c1617933
Andreas Thune <andy_19955@hotmail.com>
2026-03-07 23:04:57 +0100

    Create toaster that displays notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
+import Toast from "@/components/Toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
-        <CartProvider>{children}</CartProvider>
+        <CartProvider>
+          {children}
+          <Toast />
+        </CartProvider>
       </body>
     </html>
   );
