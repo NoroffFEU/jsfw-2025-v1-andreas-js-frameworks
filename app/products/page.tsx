4c9722684d487a512a5f2fb16b8ec1d542c9b004
Andreas Thune <andy_19955@hotmail.com>
2026-03-14 23:55:23 +0100

    Reduce page title size and change color to white

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,7 +7,7 @@ export default function AllProductsPage() {
     <>
       <main className="flex-1 flex justify-center">
         <div className="max-w-7xl w-full pt-12 pb-16 px-6">
-          <h1 className="text-3xl font-bold mb-6">All Products</h1>
+          <h1 className="text-3xl font-bold mb-6 text-white">All Products</h1>
           <FetchProducts />
         </div>
       </main>
